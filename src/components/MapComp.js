093fef6e5e4061e0441de4e8a0410a97eabde439
Carmine Corrado <carmine7188@libero.it>
2020-12-03 11:50:51 +0100

    fixed style in header

--- src/components/MapComp.js
+++ src/components/MapComp.js
@@ -1,86 +1,83 @@
 import React, { useContext, useEffect } from "react";
-import 'leaflet/dist/leaflet.css'
-import '../assets/MapComp.css'
+import "leaflet/dist/leaflet.css";
+import "../assets/MapComp.css";
 import { AppContext } from "../context/AppContext";
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet"
-import {Card, Button, Breadcrumb} from 'react-bootstrap'
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Card, Button, Breadcrumb } from "react-bootstrap";
 
 const MapComp = () => {
+  const { data } = useContext(AppContext);
 
-  const { data } = useContext(AppContext)
-  
- 
+  const arrCitta = [];
+  const coordsCitta = [];
 
-  const arrCitta = []
-  const coordsCitta = []
-  
-
-  const filtFunc = data.rows.map(x =>{
-    return arrCitta.push(x.places[0].name) ,coordsCitta.push(x.places[0].position.coords) 
+  const filtFunc = data.rows.map((x) => {
+    return (
+      arrCitta.push(x.places[0].name),
+      coordsCitta.push(x.places[0].position.coords)
+    );
   });
-  
-  const filt = arrCitta.filter((x, index) =>{
-    return arrCitta.indexOf(x) === index;
-  })
-  console.log(`filtro : ${filt}`)
-  coordsCitta.map(el=>{
-    console.log(el)
-  })
 
+  const filt = arrCitta.filter((x, index) => {
+    return arrCitta.indexOf(x) === index;
+  });
+  console.log(`filtro : ${filt}`);
+  coordsCitta.map((el) => {
+    console.log(el);
+  });
 
   return (
     <>
-   
-  
-    <div className="mapComp">
-
-      <Card className='mapComp_cardContainer'>
-        <div className="mapComp_cardContainer_mapWrapper">
-      <MapContainer style={{width: 800, height:300, marginTop:30}}center={coordsCitta[0]} zoom={7} scrollWheelZoom={false}>
-      
-      
-      <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    {coordsCitta.map(coord=>(<Marker position={coord}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-    ))}
-    {/* <Marker position={(position)}>
+      <div className="mapComp container-fluid">
+        <Card className="mapComp_cardContainer">
+          <Card.Body>
+            <div className="mapComp_cardContainer_mapWrapper">
+              <MapContainer
+                style={{ width: '100%', height: 300, marginTop: 30 }}
+                center={coordsCitta[0]}
+                zoom={7}
+                scrollWheelZoom={false}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {coordsCitta.map((coord) => (
+                  <Marker position={coord}>
+                    <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                  </Marker>
+                ))}
+                {/* <Marker position={(position)}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
       </Popup>
     </Marker> */}
-  </MapContainer>
-        </div>
-     <Card.Body>
-      <Card.Title className="cardTitle">{data.title}</Card.Title>
-        <Breadcrumb className="breadCrumb">
-            {filt.map(el =>(
-              <Breadcrumb.Item active>{el}</Breadcrumb.Item>
-            ))}
-            
-        </Breadcrumb>
-        <i><h5>Dal {data.dateFrom} al {data.dateTo} </h5></i>
-        <i><h5>2 adulti</h5></i>
-        <i><h5>7 Giorni - 6 Notti</h5></i>
-      
-      </Card.Body>
-      </Card>
+              </MapContainer>
+            </div>
+            <Card.Title className="cardTitle">{data.title}</Card.Title>
+            <Breadcrumb className="breadCrumb">
+              {filt.map((el) => (
+                <Breadcrumb.Item active>{el}</Breadcrumb.Item>
+              ))}
+            </Breadcrumb>
+            <i>
+              <h5>
+                Dal {data.dateFrom} al {data.dateTo}{" "}
+              </h5>
+            </i>
+            <i>
+              <h5>2 adulti</h5>
+            </i>
+            <i>
+              <h5>7 Giorni - 6 Notti</h5>
+            </i>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+};
 
-
-    </div>
-
-
-
-
-
-
-  </>
-  )
-}
-
-export default MapComp
+export default MapComp;
