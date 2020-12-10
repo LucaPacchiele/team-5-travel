4610a9e5db4cf3782a79b80259413d79b36a1f72
LucaPacchiele <luca.pacchiele2@gmail.com>
2020-12-10 22:13:22 +0100

    chatbot completed

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -18,10 +18,30 @@ const Chatbot = () => {
   const [arrow, setArrow] = useState(true)
   const [messaggi, setMessaggi] = useState([])
   const [text, setText] = useState("")
+  const [sent, setSent] = useState(false)
+  const [msgExit, setMsgExit] = useState(false)
+  const messagesEndRef = React.createRef()
+
+  const rispRand = ["Non era meglio se rimanevi a Siracusa?",
+    "Non lo so, prova a fare un console.log()",
+    "Il computer si riavvierà tra qualche secondo...",
+    "Dovresti implementare le emoticon ;)","Dovresti implementare le emoticon ;)"]
+  const popupSent = {
+    justifyContent: "flex-start",
+    display: "flex", textAlign: "left"
+  }
+  const bgSent = { backgroundColor: "#c1ffc1" }
+  const bgReceived = { backgroundColor: "#ffe9c1" }
+  const popupReceived = {
+    justifyContent: "flex-end",
+    display: "flex", textAlign: "right"
+  }
 
   const submitForm = (e) => {
     e.preventDefault();
-    console.log("text", text)
+    if (text === "sconto") {
+      setMsgExit(true)
+    }
     setMessaggi(prevState => {
       const newState = [...prevState]
       newState.push({
@@ -30,51 +50,62 @@ const Chatbot = () => {
       })
       return newState
     })
-
+    setSent(true)
     setText("")
-    setText("aaaaaaaaaaaaaaaa")
-    console.log("textrisp", text)
+  }
+
+  const rispondeBot = () => {
+    setSent(false)
     setMessaggi(prevState => {
       const newState = [...prevState]
       newState.push({
-        body: "Mi spiace, non capisco",
+        body: rispRand[Math.floor(Math.random() * (rispRand.length - 1 - 0)) + 0],
         sent: false
       })
       return newState
     })
-    setText("")
   }
 
   useEffect(() => {
-    console.log("messaggi", messaggi)
-  }, [messaggi])
+    let timer = setTimeout(() => rispondeBot(), 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [sent]
+  )
+
+  useEffect(() => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+  }, [messaggi]
+  )
 
 
   return (
 
-    <div className="Chatbot">
+    <div className="Chatbot" style={msgExit ? { display: "none" } : { display: "flex" }}>
 
       <div className="ChatbotContainer">
-        <Accordion defaultActiveKey="999999">
+        <Accordion>
           <Card>
 
             <Accordion.Collapse eventKey="999999">
               <Card.Body>
                 <div className="listaMessaggi">
                   {messaggi.map((messaggio, index) => (
-                    <div className="msgItem" key={index}>
-                      <div style={messaggio.sent ? { textAlign: "left", color: "black" } : { textAlign: "right", color: "#004a4a" }}>
+                    <div className="msgItem" style={messaggio.sent ? popupSent : popupReceived} key={index}>
+                      <div className="popupMsg" style={messaggio.sent ? bgSent : bgReceived}>
                         {messaggio.body}
                       </div>
                     </div>
                   ))}
+                  <div ref={messagesEndRef} />
                 </div>
                 <div className="formInput">
                   <form onSubmit={(e) => submitForm(e)} noValidate>
                     <div className="d-flex">
-                      <input type="text" placeholder="Inserisci un messaggio" className="inputMessagge"
+                      <input type="text" placeholder="Inserisci un messaggio..." className="inputMessagge"
                         name="messaggio" value={text} onChange={(e) => { setText(e.target.value) }}></input>
-                      <button className="inputButton" type="submit" name="confirm">OK</button>
+                      <button className="inputButton" type="submit" name="confirm">Invia</button>
                     </div>
                   </form>
                 </div>
@@ -83,8 +114,10 @@ const Chatbot = () => {
             </Accordion.Collapse >
             <Accordion.Toggle variant="outline-light" eventKey="999999" onClick={() => { setArrow(!arrow) }}>
               <div className="parlaCon">
-                <div className="nomeOperatore">Parla con <strong>{data.operator.name}</strong></div>
-                <div className="arrowIcon">
+                <div className="nomeOperatore">
+                  <span className="talkWith">Parla con <strong>{data.operator.name}</strong></span></div>
+                <div className="arrowIcon"><i className="fa fa-headset headsetChatBot"></i>
+
                   {arrow ?
                     <i className="fa fa-angle-down"></i> :
                     <i className="fa fa-angle-up"></i>
