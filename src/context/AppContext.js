0fd8cf4d9befd3de308cde3167f27bf8b71c44bc
LucaPacchiele <luca.pacchiele2@gmail.com>
2020-12-11 22:55:49 +0100

    test deploy 5

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -8,7 +8,7 @@ export const AppContext = createContext()
 const AppProvider = ({ children }) => {
  
   // const URL = "http://51.77.82.133:86/api/quotations/QUO_5fb3acb3a0f18"
-  const URL = "/search"
+  const URL = "https://elegant-allen-8ecac7.netlify.app/QUO_5fb3acb3a0f18"
 
   const [data, setData] = useState()
 
